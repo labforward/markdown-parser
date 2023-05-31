02d204f1619afd7663a77d04881f4a90648ee722
Ignatius Reza <lyoneil.de.sire@gmail.com>
2023-05-31 08:51:08 +0200

    fix type issues

---
+++ src/types/micromark-util-types/index.d.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+export type * from "micromark-util-types";
+
+declare module "micromark-util-types" {
+  export interface ContainerState extends ContainerState {
+    indentation: number;
+  }
+
+  export interface TokenTypeMap extends TokenTypeMap {
+    bangInterpolation: "bangInterpolation";
+    grid: "grid";
+    gridContainer: "gridContainer";
+    gridContainerNewline: "gridContainerNewline";
+    gridContainerPercentSign: "gridContainerPercentSign";
+    gridPrefix: "gridPrefix";
+    gridProps: "gridProps";
+    interpolation: "interpolation";
+    interpolationTemp: "interpolationTemp";
+  }
+}
